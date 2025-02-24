41d93454e18c2b2c2995ad9fd5254f51fb9a8913
ikingfrank3 <aomano3@gmail.com>
2025-02-23 22:20:05 -0500

    final commit 3

--- Test-WebApplication/Test-WebApplication/Pages/Settings.cshtml.cs
+++ Test-WebApplication/Test-WebApplication/Pages/Settings.cshtml.cs
@@ -4,9 +4,15 @@ namespace Test_WebApplication.Pages
 {
     public class SettingsModel : PageModel
     {
+        [BindProperty]
         public string username { get; set; }
+
+        [BindProperty]
         public string password { get; set; }
-        public void OnGet()
+        public void OnGet() 
+        {
+            username = "admin";
+            password = "admin";
         }
     }
 }
